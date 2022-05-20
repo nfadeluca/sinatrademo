d2e566ce0c9caa1fb5cb8216ba552002934f43d3
Nick De Luca <nfadeluca@gmail.com>
2022-05-20 19:34:42 -0400

    Exercise complete

--- app.rb
+++ app.rb
@@ -1,5 +1,25 @@
 require 'sinatra'
 
+# Reading contents from file into array
+def read_file(file)
+  file = File.open(file, "r")
+  contents = file.readlines
+  file.close
+  return contents
+end
+
+x = read_file("cake.list")
+
+# Adding number of cake to front of each cake's name
+num = 1
+x.each { 
+  |x|
+  x.insert(0, num.to_s+": ")
+  num+=1
+}
+
 get '/' do
-  "Hello, world"
+  # Replace /n with br/ so newline visible on webpage
+  x.to_s.gsub('\n', '<br/>')
+  x.join("<br/>")
 end
