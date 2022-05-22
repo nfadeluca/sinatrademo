f64698b431969c17f0b05430d166a7fa25c99bbb
Nick De Luca <nfadeluca@gmail.com>
2022-05-22 18:24:37 -0400

    Some touchups

--- app.rb
+++ app.rb
@@ -5,21 +5,22 @@ def read_file(file)
   file = File.open(file, "r")
   contents = file.readlines
   file.close
-  return contents
+  contents
 end
 
-lines = read_file("cake.list")
-
-# Adding number of cake to front of each cake's name
-num = 1
-lines.each { 
-  |lines|
-  lines.insert(0, num.to_s+": ")
-  num+=1
-}
-
 get '/' do
+
+  lines = read_file("cake.list")
+  # Adding number of cake to front of each cake's name
+  lines.each.with_index(1) do |line, index|
+    line.insert(0, index.to_s+": ")
+    # Aligning all indices
+    if (index.between?(1,9)==true) then line.insert(0, "&nbsp&nbsp") end
+    if (index.between?(10,99)==true) then line.insert(0, "&nbsp") end
+  end
+
   # Replace /n with br/ so newline visible on webpage
   lines.to_s.gsub('\n', '<br/>')
   lines.join("<br/>")
+
 end
