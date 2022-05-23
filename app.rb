7857a1f092e469c35efb860d4db9f07b5fa8a1b7
Nick De Luca <nfadeluca@gmail.com>
2022-05-23 18:05:28 -0400

    Attempting to pipe cakes.json into index.erb

--- app.rb
+++ app.rb
@@ -1,40 +1,39 @@
 require 'sinatra'
 require 'json'
 
+# Reading contents from file into array
+def read_file(file)
+  file = File.open(file, "r")
+  contents = file.readlines
+  file.close
+  contents
+end
+
+get '/' do
+
+  lines = read_file("cake.list")
+  # Adding number of cake to front of each cake's name
+  lines.each.with_index(1) do |line, index|
+    line.insert(0, index.to_s+": ")
+    # Aligning all indices
+    line.insert(0, "&nbsp&nbsp") if (index.between?(1,9))
+    line.insert(0, "&nbsp") if (index.between?(10,99))
+  end
+
+  # Replace /n with br/ so newline visible on webpage
+  lines.to_s.gsub('\n', '<br/>')
+  lines.join("<br/>")
+
+end
+
+
+
 # Creating json file from cake.list
 get '/cakes.json' do
   lines = File.open("cake.list").read.split("}\n").join("},")+"}"
   contents = JSON.parse([lines].to_s)
 end
 
-get '/' do
+get '/index' do
   erb :index
 end
-
-
-
-# # Reading contents from file into array
-# def read_file(file)
-#   file = File.open(file, "r")
-#   contents = file.readlines
-#   file.close
-#   contents
-# end
-
-
-# get '/' do
-
-#   lines = read_file("cake.list")
-#   # Adding number of cake to front of each cake's name
-#   lines.each.with_index(1) do |line, index|
-#     line.insert(0, index.to_s+": ")
-#     # Aligning all indices
-#     line.insert(0, "&nbsp&nbsp") if (index.between?(1,9))
-#     line.insert(0, "&nbsp") if (index.between?(10,99))
-#   end
-
-#   # Replace /n with br/ so newline visible on webpage
-#   lines.to_s.gsub('\n', '<br/>')
-#   lines.join("<br/>")
-
-# end
