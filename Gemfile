42b09a56276832391eb5812298ac1c020aad2388
Nick De Luca <nfadeluca@gmail.com>
2022-05-24 20:08:08 -0400

    Changed up cakes.json formatting

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 gem 'rack'
 gem 'sinatra'
 gem 'puma'
+gem 'sinatra-contrib', require: 'sinatra/reloader'
--- app.rb
+++ app.rb
@@ -9,24 +9,24 @@ def read_file(file)
   contents
 end
 
-get '/' do
+# get '/' do
 
-  lines = read_file("cake.list")
-  # Adding number of cake to front of each cake's name
-  lines.each.with_index(1) do |line, index|
-    line.insert(0, index.to_s+": ")
-    # Aligning all indices
-    line.insert(0, "&nbsp&nbsp") if (index.between?(1,9))
-    line.insert(0, "&nbsp") if (index.between?(10,99))
-  end
+#   lines = read_file("cake.list")
+#   # Adding number of cake to front of each cake's name
+#   lines.each.with_index(1) do |line, index|
+#     line.insert(0, index.to_s+": ")
+#     # Aligning all indices
+#     line.insert(0, "&nbsp&nbsp") if (index.between?(1,9))
+#     line.insert(0, "&nbsp") if (index.between?(10,99))
+#   end
 
-  # Replace /n with br/ so newline visible on webpage
-  lines.to_s.gsub('\n', '<br/>')
-  lines.join("<br/>")
+#   # Replace /n with br/ so newline visible on webpage
+#   lines.to_s.gsub('\n', '<br/>')
+#   lines.join("<br/>")
 
-  puts lines
+#   puts lines
 
-end
+# end
 
 
 
@@ -34,16 +34,20 @@ end
 get '/cakes.json' do
   lines = read_file("cake.list")
   lines.each.with_index(1) do |line, index|
-    line.insert(0, "{\"name\":\"")
+    line.insert(0, index.to_s+":\"")
+    line.insert(0, "{")
   end
-  lines.each.with_index(1) do |line, index|
-    line.insert(-2, "\"}")
+  lines.each.with_index do |line, index|
+    if index == lines.length-1
+      line.insert(-1, "\"}")
+    else
+      line.insert(-2, "\"},")
+    end
   end
-
-  contents = JSON.parse(lines.to_s)
+  data = JSON.parse(lines.to_s)
 end
 
 # Creating index route
-get '/index' do
+get '/' do
   erb :index
 end
