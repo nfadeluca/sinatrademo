1adbf059794a683cf03841805a3d0e6b759aa340
Nick De Luca <nfadeluca@gmail.com>
2022-05-29 13:18:27 -0400

    Can now view all cake names on index from psql database

--- app.rb
+++ app.rb
@@ -3,20 +3,20 @@ require 'json'
 require 'pg'
 
 
-get '/' do
+get '/cakes.json' do
   arr_cakes = []
-
-  begin
-    # Connecting to the database "cakes"
-    connection = PG.connect :dbname => 'cakes', :user => 'postgres'
-    # Reading from the database into s_cakes
-    s_cakes = connection.exec 'SELECT * FROM cakes'
-    # Pushing data from cakes db into arr_cakes
-    s_cakes.each do |cake|
-      arr_cakes.push({ id: cake['cake_id'], name: cake['cake_name'] })
-    end
-
+  # Connecting to the database "cakes"
+  connection = PG.connect :dbname => 'cakes', :user => 'postgres'
+  # Reading from the database into s_cakes
+  s_cakes = connection.exec 'SELECT * FROM cakes'
+  # Pushing data from cakes db into arr_cakes
+  s_cakes.each do |cake|
+    arr_cakes.push({ id: cake['cake_id'], name: cake['cake_name'] })
   end
+  arr_cakes.to_json
+end
 
+# Index page to show all of the cakes
+get '/' do
   erb :index
 end
