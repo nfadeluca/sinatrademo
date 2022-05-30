4f8fae8b91626ac48c55c6cd9818df758c0f9001
Nick De Luca <nfadeluca@gmail.com>
2022-05-29 22:12:35 -0400

    Removed index:erb

--- app.rb
+++ app.rb
@@ -3,20 +3,26 @@ require 'json'
 require 'pg'
 
 
-get '/cakes.json' do
-  arr_cakes = []
-  # Connecting to the database "cakes"
-  connection = PG.connect :dbname => 'cakes', :user => 'postgres'
-  # Reading from the database into s_cakes
-  s_cakes = connection.exec 'SELECT * FROM cakes'
-  # Pushing data from cakes db into arr_cakes
-  s_cakes.each do |cake|
-    arr_cakes.push({ id: cake['cake_id'], name: cake['cake_name'] })
-  end
-  arr_cakes.to_json
+arr_cakes = []
+# Connecting to the database "cakes"
+connection = PG.connect :dbname => 'cakes', :user => 'postgres'
+# Reading from the database into s_cakes
+s_cakes = connection.exec 'SELECT * FROM cakes'
+# Pushing data from cakes db into arr_cakes
+s_cakes.each do |cake|
+  arr_cakes.push({ id: cake['cake_id'], name: cake['cake_name'] })
 end
 
 # Index page to show all of the cakes
 get '/' do
-  erb :index
+  arr_cakes.to_json
+end
+
+# Showind based off of cake's id
+get '/cakes/:id' do
+  cakeid = params["id"].to_s
+  cake_to_show = ""
+  arr_cakes.each {|cake|
+    
+  }
 end
