8cb9d96db82813e8967e9900f615fcbca271f720
Nick De Luca <nfadeluca@gmail.com>
2022-05-31 08:22:03 -0400

    Update route complete

--- app.rb
+++ app.rb
@@ -34,3 +34,18 @@ post '/' do
   connection.exec("INSERT INTO cakes VALUES (" + "\'" + body["id"] + "\'" + ", " + "\'" +body["name"]+"\'"+ ");")
   arr_cakes.to_json
 end
+
+# Creating route to update cake based off of id given HTTP request with JSON body
+put '/cakes/:id' do
+  # Param passed into URL
+  cakeid = params["id"]
+  body = JSON.parse(request.body.read)
+  cake_to_edit = ""
+  arr_cakes.each { |cake| cake_to_edit = cake if cakeid == cake[:id] }
+  cake_to_edit[:id] = body["id"]
+  cake_to_edit[:name] = body["name"]
+  # Updating database from postgres
+  connection.exec("UPDATE cakes SET cake_name = " +"\'"+ body["name"] +"\'"+ " WHERE cake_id = " + "\'"+cakeid+"\';")
+  connection.exec("UPDATE cakes SET cake_id = " +"\'"+ body["id"] +"\'"+ " WHERE cake_id = " + "\'"+cakeid+"\';")
+  cake_to_edit.to_json
+end
